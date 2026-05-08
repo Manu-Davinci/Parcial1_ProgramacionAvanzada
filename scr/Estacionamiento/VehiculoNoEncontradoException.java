c1d27a321eda78b18624a9dd9694d08e88bbea76
Manu-Davinci <manuel.portillacusqu@davinci.edu.ar>
2026-05-08 16:39:56 -0300

    Create VehiculoNoEncontradoException.java

---
+++ scr/Estacionamiento/VehiculoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package scr.Estacionamiento;
+
+public class VehiculoNoEncontradoException extends Exception {
+
+    public VehiculoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
