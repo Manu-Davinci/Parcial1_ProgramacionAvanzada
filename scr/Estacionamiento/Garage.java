0aa2be2226f350005fd29abd83153e01604a39ed
Manu-Davinci <manuel.portillacusqu@davinci.edu.ar>
2026-05-08 16:22:21 -0300

    Update Garage.java

---
+++ scr/Estacionamiento/Garage.java
@@ -0,0 +1,29 @@
+package scr.Estacionamiento;
+
+import java.util.ArrayList;
+
+public class Garage {
+
+    //Atributos  
+    private int capacidadMaxima;
+    private ArrayList<Vehiculo> vehiculos;
+
+    //Constructor
+    public Garage(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        vehiculos = new ArrayList<>();
+    }
+
+    //Metodos
+     public int espacioOcupadoTotal() {
+
+        int total = 0;
+
+        for (Vehiculo v : vehiculos) {
+            total += v.espacioOcupado();
+        }
+
+        return total;
+    }
+    
+}
