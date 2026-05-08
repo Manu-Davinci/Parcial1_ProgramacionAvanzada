9c2e205ec39543db6a85d91502d54a95b1cd791c
Manu-Davinci <manuel.portillacusqu@davinci.edu.ar>
2026-05-08 16:35:06 -0300

    Create GarageLlenoException.java

---
+++ scr/Estacionamiento/GarageLlenoException.java
@@ -0,0 +1,8 @@
+package scr.Estacionamiento;
+
+//Este mensaje se usa cuando se ingresa un vehivulo y el garage ya esta lleno
+public class GarageLlenoException {
+    public GarageLlenoException(String mensaje) {
+        super(mensaje);
+    }
+}
