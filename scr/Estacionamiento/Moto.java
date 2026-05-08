be7c456a0feb44a6dc574cdd78caf66114e50550
Manu-Davinci <manuel.portillacusqu@davinci.edu.ar>
2026-05-08 15:43:58 -0300

    Create Moto.java

---
+++ scr/Estacionamiento/Moto.java
@@ -0,0 +1,24 @@
+package scr.Estacionamiento;
+
+public class Moto extends Vehiculo {
+
+    //Constructor
+    public Moto(String patente, String marca, String modelo, int horas) {
+        super(patente, marca, modelo, horas);
+    }
+    
+    @Override 
+    public double calcularCosto() {
+        return horas * 700;
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("Tipo: Moto");
+        System.out.println("Patente: " + patente);
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Horas: " + horas);
+        System.out.println("Costo Total: $" + calcularCosto());
+    }
+}
